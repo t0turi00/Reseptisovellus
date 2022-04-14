a432202939d2201d2e1d0096ea602c7300a4c084
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2022-04-14 11:09:06 +0300

    Koodin edistämistä
    v 0.2.3

--- r13_mob_proj_ui/app/src/main/java/com/example/r13_mob_proj_ui/MainActivity.kt
+++ r13_mob_proj_ui/app/src/main/java/com/example/r13_mob_proj_ui/MainActivity.kt
@@ -3,32 +3,34 @@ package com.example.r13_mob_proj_ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.r13_mob_proj_ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-
         binding.imageButton.setOnClickListener {
             startAc1()
         }
-
         binding.imageButton2.setOnClickListener {
             startAc2()
         }
-
         binding.imageButton3.setOnClickListener {
             startAc3()
         }
-
         binding.imageButton4.setOnClickListener {
             startAc4()
+        }
+        binding.buAdd.setOnClickListener {
+            startAdder()
         }
     }
     fun startAc1() {
@@ -45,6 +47,10 @@ class MainActivity : AppCompatActivity() {
     }
     fun startAc4() {
         val intent = Intent(this, reseptiview4::class.java)
+        startActivity(intent)
+    }
+    fun startAdder() {
+        val intent = Intent(this, AddDataView::class.java)
         startActivity(intent)
     }
 }
